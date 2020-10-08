7eaedf1f4f083d74de82c30ba51d398f114b6e32
Shamada81 <71727577+Shamada81@users.noreply.github.com>
2020-10-08 04:45:27 +0300

    Create User.php

---
+++ User.php
@@ -0,0 +1,51 @@
+<?php
+
+class User{
+  static function addUser(){
+    $mysqli = mysqli_connect("localhost", "levadny02_train", "olUuFf&8", "levadny02_train");
+    $name = $_POST['name'];
+    $lastname = $_POST['lastname'];
+    $email = mb_strtolower(trim($_POST['email']));
+    $pass = trim($_POST['pass']);
+    $pass = password_hash($pass, PASSWORD_DEFAULT);
+    $result = $mysqli->query("SELECT * FROM `users` WHERE `email`='$email'");
+    if ($result->num_rows){
+      echo "exist";
+    }else{
+      $mysqli->query("INSERT INTO `users`(`name`, `lastname`, `email`, `pass`) VALUES ('$name','$lastname','$email','$pass')");
+      echo "success";
+    }
+  }
+  static function authUser(){
+    $mysqli = new mysqli("localhost", "levadny02_train", "olUuFf&8", "levadny02_train");
+    $email = mb_strtolower(trim($_POST['email']));
+    $pass = trim($_POST['pass']);
+    $result = $mysqli->query("SELECT * FROM `users` WHERE `email`='$email'");
+    $row = $result->fetch_assoc();
+    if (password_verify($pass,$row['pass'])){
+      $_SESSION["name"] = $row["name"];
+      $_SESSION["lastname"] = $row["lastname"];
+      $_SESSION["email"] = $row["email"];
+      $_SESSION["id"] = $row["id"];
+      echo "success";
+    }else{
+      echo "error";
+    }
+  }
+  static function changeUserData(){
+    $mysqli = new mysqli("localhost", "levadny02_train", "olUuFf&8", "levadny02_train");
+    $value = $_POST['value'];
+    $item = $_POST['item'];//здесь либо name либо lastname
+    $userId = $_SESSION['id'];
+    $mysqli->query("UPDATE `users` SET `$item`='$value' WHERE `id`='$userId'");
+    $_SESSION[$item] = $value;
+    /* Меняет данные пользователя (аналог lk_obr.php) */ 
+  }
+  static function getUsers(){
+    /* Пойду в БД и достану список */
+  }
+  static function getUser($userId){
+    /* Пойду в БД и достану пользователя по его ID */
+  }
+}
+?>
